0ce917af57bc9129ff9dd7de9b8025ca33fdf989
9R3A <man9r3a@gmail.com>
2024-12-20 19:27:41 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,23 @@
+@echo off
+
 cd
+
+call mode con: cols=100 lines=20
+
+title Installing Libs...
 
 call npm install discord.js dotenv
 
 color 2
 
-node bot.js
+echo download done !
 
-pause
+timeout /t 5
+
+call mode con: cols=100 lines=30
+
+color 3
+
+title Made By 9R3A ^<3
+
+node index.js
